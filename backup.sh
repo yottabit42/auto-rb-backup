766a615823d1d361db04bc89dd17331623ea87a3
Jacob McDonald <jacob@mcawesome.org>
2018-11-29 13:46:00 -0600

    Add Github link, and consistency clean-up.

--- backup.sh
+++ backup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-# Backup multiple Routerboard devices to local script files.
+# Backup multiple RouterOS devices to local script files.
 #
 # Author: Jacob McDonald.
-# Revision: 181129a.
+# Revision: 181129b.
+#
+# See https://github.com/yottabit42/auto-rb-backup for full details.
 #
 # Requirements:
 # 1. Local bash shell.
 # 2. Local SSH client.
-# 3. SSH service enabled and reachable on Routerboard devices.
+# 3. SSH service enabled and reachable on RouterOS devices.
 # 4. SSH private key, without password, installed on the local host.
-# 5. SSH public key installed for a user on the Routerboard devices.
-# 6. Routerboard device user must have permission to run '/export'.
+# 5. SSH public key installed for a user on the RouterOS devices.
+# 6. RouterOS device user must have permission to run '/export'.
 # 7. Optional: install into crontab for saving the backups routinely.
 
 # IPs of Routerboards to backup with the SSH public key installed.
